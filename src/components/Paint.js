dda7d04bbee837138caf4c8652e536d1c559131c
CRCotton <carly.rae.cotton@gmail.com>
2022-05-15 22:02:35 -0700

    Changed buttons on Paint card to help with mobile phone capatability.

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -6,17 +6,17 @@ const Paint = ({paint, onAdd, onSubtract}) => {
     return(
         <><div className="content">
             <div className="header left floated" key="{paint.key}">{paint.colour}</div>
-            <div className="header right floated"><i className="small tag icon"></i>#{paint.key}</div>
-        </div><div className="centered content" style={{ backgroundColor: paint.colour }}><i className="circular icon inverted tint"></i></div><div className="extra content">
-                <span className="left floated">
-                    <button className="ui icon button" onClick={() => onAdd(paint.key)}><i className="plus green icon"></i></button>
-                </span>
-                <span className="right floated">
-                    <button className="ui icon button" onClick={() => onSubtract(paint.key)}><i className="minus red icon"></i></button>
-                </span>
-            </div></>
-    );
-    
+            <div className="header right floated"><i className="small tags icon"></i>{paint.key}</div>
+        </div><div className="centered content" style={{ backgroundColor: paint.colour }}><i className="circular icon inverted tint"></i></div>
+        <div className="extra content">
+            <div class="ui large buttons">
+                <button class="ui button" onClick={() => onAdd(paint.key)}><i className="plus green icon"></i></button>
+                <div class="or"></div>
+                <button class="ui button" onClick={() => onSubtract(paint.key)}><i className="minus red icon"></i></button>
+            </div>
+        </div>
+                </>
+                );
 }
+                export default Paint;
 
-export default Paint;
